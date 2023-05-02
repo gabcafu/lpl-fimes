15c58dcd058958e6b3f8897ce38f4e8d1db24c54
gabcafu <89201527+gabcafu@users.noreply.github.com>
2023-05-02 13:28:13 -0300

    Add files via upload

---
+++ ISeries.ts
@@ -0,0 +1,10 @@
+export interface ISeries{
+  nome: string;
+  lancamento: string;
+  temporadas: string;
+  classificacao: number;
+  cartaz: string;
+  generos: string[];
+  pagina?: string; //** a ? indica que o campo nao é obrigatorio*/
+  favorito: boolean;
+}
